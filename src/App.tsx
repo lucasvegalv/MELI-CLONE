472b184efdefa0a8d97abe44269846acac0dad75
lucasvegalv <lucasvega02@gmail.com>
2024-08-26 23:30:04 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='m-0 flex justify-center items-center'>
+
+      <h1 className='font-extrabold'>hola</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
